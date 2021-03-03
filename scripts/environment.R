676f9e40bbf429ec836030d9a2dad33a084948ed
Delwen Franzen <delwen@franzen.fm>
2021-03-03 10:33:00 +0100

    Add environment configuration

---
+++ scripts/environment.R
@@ -0,0 +1,33 @@
+# Adapted from Maia Salholz-Hillel (https://github.com/maia-sh/trn-pop/blob/master/R/environment.R)
+
+# Install and load packages for R scripts -----------------------------------------------
+
+cran_pkgs <- c(
+
+  # config
+  "ConfigParser",
+  
+  # General
+  "tidyverse", "xml2", "httr", "jsonlite", "here"
+
+)
+
+
+to_install <- cran_pkgs[!cran_pkgs %in% installed.packages()]
+
+if (length(to_install) > 0) {
+  install.packages(to_install, deps = TRUE, repos = "https://cran.r-project.org")
+}
+
+invisible(lapply(c(cran_pkgs), library, character.only = TRUE))
+
+
+# Specify configuration  -----------------------------------------------
+cfg <- ConfigParser$new()
+cfg$read("config.ini")
+
+# Specify data directory
+data_dir <- cfg$get("data", NA, "paths")
+
+# Specify API key
+apikey <- cfg$get("api_key", NA, "login")
